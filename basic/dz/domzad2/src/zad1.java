66443cb86ce296c60c50ec890a2f121e6d056694
SnappyWave <showerpool@gmail.com>
2024-12-17 01:35:48 +0300

    .

---
+++ basic/dz/domzad2/src/zad1.java
@@ -0,0 +1,11 @@
+public class zad1 {
+    public static void main(String[] args) {
+        double[] radii = {1, 0.5, 0.2};
+        double density = 0.7;
+        double weight = 0;
+        for (double r : radii) {
+            weight += (4.0 / 3) * Math.PI * Math.pow(r, 3) * density;
+        }
+        System.out.println(weight);
+    }
+}
---
+++ basic/dz/domzad2/src/zad2.java
@@ -0,0 +1,7 @@
+public class zad2 {
+    public static void main(String[] args) {
+        double a = 5, b = 3, c = 9;
+        double result = Math.abs(a - b) / Math.pow(a + b, 3) - Math.sqrt(c);
+        System.out.println(result);
+    }
+}
