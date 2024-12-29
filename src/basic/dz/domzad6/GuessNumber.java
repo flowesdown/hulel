81627603877132e884acc69746dc317d8c966da5
SnappyWave <showerpool@gmail.com>
2024-12-30 00:48:40 +0300

    Was added the user interaction

--- src/basic/dz/domzad6/GuessNumber.java
+++ src/basic/dz/domzad6/GuessNumber.java
@@ -7,6 +7,10 @@ public class GuessNumber {
     public static void main(String[] args) {
         int num = new Random().nextInt(11);
         Scanner sc = new Scanner(System.in);
-        while (sc.nextInt() != num);
+        System.out.println("Guess the number");
+        while (sc.nextInt() != num){
+            System.out.println("Guess the number again");
+        };
+        System.out.println("Good job");
     }
 }
